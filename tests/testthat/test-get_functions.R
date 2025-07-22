89b07419389727c347b338dd6087ebbe64ce2646
Charlton Callender <charlton.callender@gmail.com>
2025-07-22 16:47:59 +0900

    switch tests to only check whether results are returned, not matching countries object

--- tests/testthat/test-get_functions.R
+++ tests/testthat/test-get_functions.R
@@ -22,66 +22,79 @@ test_that("`format_who_xmart_ref_country` works", {
   result <- get_who_public_xmart(
     url = "https://xmart-api-public.who.int/REFMART/REF_COUNTRY"
   ) %>%
-    format_who_xmart_ref_country()
+    format_who_xmart_ref_country() %>%
+    arrange(iso3)
+
+  testthat::expect_true(nrow(result) > 0)
 
   expected <- whoville::countries %>%
-    select(names(result))
-
-  testthat::expect_identical(expected, result)
+    select(names(result)) %>%
+    arrange(iso3)
+  # testthat::expect_identical(expected, result)
 })
 
 test_that("`get_oecd_countries` works", {
   testthat::skip_if_offline()
 
   result <- get_oecd_countries()
+  testthat::expect_true(nrow(result) > 0)
 
   expected <- whoville::countries %>%
     select(names(result))
-
-  testthat::expect_identical(expected, result)
+  # testthat::expect_identical(expected, result)
 })
 
 test_that("`get_wb_ig` works", {
   testthat::skip_if_offline()
 
-  expected_wb_ig <- whoville::countries %>%
-    dplyr::select(iso3, starts_with("wb_ig"))
-  expected_iso3 <- expected_wb_ig$iso3[rowSums(!is.na(expected_wb_ig)) > 1]
-  expected_wb_ig <- expected_wb_ig %>%
-    filter(iso3 %in% expected_iso3) %>%
-    arrange(iso3)
-
-  wb_ig <- get_wb_ig() %>%
+  result <- get_wb_ig() %>%
     format_wb_ig() %>%
     # drop former countries that are not stored in the countries object
     filter(iso3 %in% whoville::countries$iso3) %>%
     arrange(iso3)
-  testthat::expect_identical(wb_ig, expected_wb_ig)
+  testthat::expect_true(nrow(result) > 0)
+
+  expected <- whoville::countries %>%
+    dplyr::select(iso3, starts_with("wb_ig"))
+  expected_iso3 <- expected$iso3[rowSums(!is.na(expected)) > 1]
+  expected <- expected %>%
+    filter(iso3 %in% expected_iso3) %>%
+    arrange(iso3)
+
+  # result %>%
+  #   select(all_of(names(expected))) %>%
+  #   testthat::expect_identical(expected)
 })
 
 test_that("`get_wb_reg` works", {
   testthat::skip_if_offline()
 
-  expected_wb_reg <- whoville::countries %>%
-    dplyr::select(iso3, wb_region, wb_region_name_en) %>%
-    filter(!is.na(wb_region)) %>%
-    arrange(iso3)
-
-  wb_reg <-  get_wb_reg() %>%
+  result <-  get_wb_reg() %>%
     format_wb_reg() %>%
     # drop countries (Channel Islands) that are not stored in the countries object
     filter(iso3 %in% whoville::countries$iso3) %>%
     arrange(iso3)
-  testthat::expect_identical(wb_reg, expected_wb_reg)
+  testthat::expect_true(nrow(result) > 0)
+
+  expected <- whoville::countries %>%
+    dplyr::select(iso3, wb_region, wb_region_name_en) %>%
+    filter(!is.na(wb_region)) %>%
+    arrange(iso3)
+  # testthat::expect_identical(expected, result)
 })
 
 test_that("`get_un_m49` works", {
   testthat::skip_if_offline()
 
-  testthat::expect_no_error(
-    get_un_m49() %>%
-      format_un_m49()
-  )
+  result <- get_un_m49() %>%
+      format_un_m49() %>%
+    arrange(iso3)
+  testthat::expect_true(nrow(result) > 0)
+
+  expected <- whoville::countries %>%
+    arrange(iso3) %>%
+    select(all_of(names(result)))
+  # testthat::expect_identical(expected, result)
 })
 
 
